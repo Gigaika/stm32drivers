4dc21dd3001f86e1fdce14410262acad8411532a
Aleksi <aleksi.maki-penttila@tuni.fi>
2020-08-20 01:55:07 +0300

    hts221

--- HTS221/hts221.c
+++ HTS221/hts221.c
@@ -4,34 +4,118 @@
 
 #include "hts221.h"
 
-HTS221_Init(HTS221_Obj *obj, Peripheral_IO_Object *io) {
-    obj->SAD = 0x5F;
-    obj->registers = 0x00;
-    obj->IO = io;
+void HTS221_Read_Reg_Cplt_Callback(HTS221_Obj *obj) {
+    switch(obj->state) {
+        case HTS221_INITIALIZING:
+        {
+            obj->calibrations.T0_degC = ((uint16_t)obj->registers.CALIB_0TOF[2] | (((uint16_t)obj->registers.CALIB_0TOF[5] & 0x3) << 8)) >> 3;
+            obj->calibrations.T1_degC = ((uint16_t)obj->registers.CALIB_0TOF[3] | ((((uint16_t)obj->registers.CALIB_0TOF[5] >> 2) & 0x3) << 8)) >> 3;
+            obj->calibrations.T0_out = ((uint16_t)obj->registers.CALIB_0TOF[12] | ((uint16_t)obj->registers.CALIB_0TOF[13] << 8));
+            obj->calibrations.T1_out = ((uint16_t)obj->registers.CALIB_0TOF[13] | ((uint16_t)obj->registers.CALIB_0TOF[14] << 8));
+            obj->calibrations.H0_rH = ((uint16_t)obj->registers.CALIB_0TOF[0] / 2);
+            obj->calibrations.H1_rH = ((uint16_t)obj->registers.CALIB_0TOF[1] / 2);
+            obj->calibrations.H0_out = ((uint16_t)obj->registers.CALIB_0TOF[6] | ((uint16_t)obj->registers.CALIB_0TOF[7] << 8));
+            obj->calibrations.H1_out = ((uint16_t)obj->registers.CALIB_0TOF[10] | ((uint16_t)obj->registers.CALIB_0TOF[11] << 8));
+            obj->state = HTS221_READY;
+            break;
+        }
+        case HTS221_READING:
+        {
+            int16_t T0_degC = obj->calibrations.T0_degC;
+            int16_t T1_degC = obj->calibrations.T1_degC,
+            int16_t T0_out = obj->calibrations.T0_out;
+            int16_t T1_out = obj->calibrations.T1_out;
+            uint16_t H0_rH = obj->calibrations.H0_rH;
+            uint16_t H1_rH = obj->calibrations.H1_rH;
+            int16_t H0_out = obj->calibrations.H0_out;
+            int16_t H1_out = obj->calibrations.H1_out;
 
-    // Todo: write proper config functions
-    obj->registers->AV_CONF = 0x3F;
-    obj->IO->write_reg(HTS221_AV_CONFR, 1, obj->registers->AV_CONF, 1);
-    obj->registers->CTRL_REG1 |= 1 << 7;
-    obj->IO->write_reg(HTS_221_CTRL_REG1, 1, obj->registers->CTRL_REG1, 1);
-
-    obj->IO->read_reg(HTS221_CALIB_0TOF, 1, obj->registers->CALIB_0TOF, 16);
-
-    obj->T0_degC = ((uint16_t)obj->registers->CALIB_0TOF[2] | (((uint16_t)obj->registers->CALIB_0TOF[5] & 0x3) << 8)) >> 3;
-    obj->T1_degC = ((uint16_t)obj->registers->CALIB_0TOF[3] | ((((uint16_t)obj->registers->CALIB_0TOF[5] >> 2) & 0x3) << 8)) >> 3;
-    obj->T0_out = ((uint16_t)obj->registers->CALIB_0TOF[12] | ((uint16_t)obj->registers->CALIB_0TOF[13] << 8));
-    obj->T1_out = ((uint16_t)obj->registers->CALIB_0TOF[13] | ((uint16_t)obj->registers->CALIB_0TOF[14] << 8));
-    obj->H0_rH = ((uint16_t)obj->registers->CALIB_0TOF[0] / 2);
-    obj->H1_rH = ((uint16_t)obj->registers->CALIB_0TOF[1] / 2);
-    obj->H0_out = ((uint16_t)obj->registers->CALIB_0TOF[6] | ((uint16_t)obj->registers->CALIB_0TOF[7] << 8));
-    obj->H1_out = ((uint16_t)obj->registers->CALIB_0TOF[10] | ((uint16_t)obj->registers->CALIB_0TOF[11] << 8));
+            int16_t raw_H = ((uint16_t)obj->registers.HUMIDITY_OUT_L | ((uint16_t)obj->registers.HUMIDITY_OUT_H << 8));
+            obj->humidity = ((obj->H1_rH - obj->H0_rH)*(raw_H-obj->H0_out))/(obj->H1_out-obj->H0_out)+obj->H0_rH;
+            int16_t raw_T = ((uint16_t)obj->registers.TEMP_OUT_L | ((uint16_t)obj->registers.TEMP_OUT_H << 8));
+            obj->temperature = ((obj->T1_degC - obj->T0_degC) * (raw_T - obj->T0_out)) / (obj->T1_out-obj->T0_out) + obj->T0_degC;
+            break;
+        }
+        default:
+            obj->state = HTS221_READY;
+            break;
+    }
 }
 
-HTS221_Read(HTS221_Obj *obj) {
-    obj->IO->read_reg(HTS221_HUMIDITY_OUT_L, 1, obj->registers->HUMIDITY_OUT_L, 4);
+void HTS221_Write_Reg_Cplt_Callback(HTS221_Obj *obj) {
+    switch(obj->state) {
+        default:
+            obj->state = HTS221_READY;
+            break;
+    }
+}
 
-    int16_t raw_H = ((uint16_t)obj->registers->HUMIDITY_OUT_L | ((uint16_t)obj->registers->HUMIDITY_OUT_H << 8));
-    obj->humidity = ((obj->H1_rH - obj->H0_rH)*(raw_H-obj->H0_out))/(obj->H1_out-obj->H0_out)+obj->H0_rH;
-    int16_t raw_T = ((uint16_t)obj->registers->TEMP_OUT_L | ((uint16_t)obj->registers->TEMP_OUT_H << 8));
-    obj->temperature = ((obj->T1_degC - obj->T0_degC) * (raw_T - obj->T0_out)) / (obj->T1_out-obj->T0_out) + obj->T0_degC;
+void HTS221_Init(HTS221_Obj *obj, Peripheral_IO_Object *io) {
+    obj->state = HTS221_INITIALIZING;
+    obj->SAD = HTS221_SAD;
+    obj->IO = io;
+
+    obj->IO->read_reg(HTS221_CALIB_0TOF, 1, obj->registers.CALIB_0TOF, 16);
+    if (obj->IO.read_reg_IT_driven) {
+        HTS221_Read_Reg_Cplt_Callback();
+    }
+}
+
+void HTS221_SetPowered(HTS221_Obj *obj, HTS221_PoweredTypeDef powered) {
+    obj->state = HTS221_CONFIGURING;
+    obj->IO->write_reg(HTS221_CTRL_REG1, 1, powered << 7, 1);
+    if (obj->IO.write_reg_IT_driven) {
+        HTS221_Write_Reg_Cplt_Callback();
+    }
+}
+
+void HTS221_SetResolution(HTS221_Obj *obj, HTS221_AVGTTypeDef tempRes, HTS221_AVGHTypeDef humRes) {
+    obj->state = HTS221_CONFIGURING;
+    uint8_t tempReg = 0;
+    tempReg |= tempRes << 3;
+    tempReg |= humRes;
+    obj->IO->write_reg(HTS221_AV_CONFR, 1, tempReg, 1);
+    if (obj->IO.write_reg_IT_driven) {
+        HTS221_Write_Reg_Cplt_Callback();
+    }
+}
+
+void HTS221_SetBDU(HTS221_Obj *obj, HTS221_BDUTypeDef bdu) {
+    obj->state = HTS221_CONFIGURING;
+    obj->IO->write_reg(HTS221_CTRL_REG1, 1, bdu << 2, 1);
+    if (obj->IO.write_reg_IT_driven) {
+        HTS221_Write_Reg_Cplt_Callback();
+    }
+}
+
+void HTS221_SetODR(HTS221_Obj *obj, HTS221_ODRTypeDef odr) {
+    obj->state = HTS221_CONFIGURING;
+    obj->IO->write_reg(HTS221_CTRL_REG1, 1, odr, 1);
+    if (obj->IO.write_reg_IT_driven) {
+        HTS221_Write_Reg_Cplt_Callback();
+    }
+}
+
+void HTS221_SetDRDY(HTS221_Obj *obj, HTS221_DRDYTypeDef drdy) {
+    obj->state = HTS221_CONFIGURING;
+    obj->IO->write_reg(HTS221_CTRL_REG3, 1, drdy << 2, 1);
+    if (obj->IO.write_reg_IT_driven) {
+        HTS221_Write_Reg_Cplt_Callback();
+    }
+}
+
+void HTS221_RequestReading(HTS221_Obj *obj) {
+    obj->state = HTS221_REQUESTING;
+    obj->IO->write_reg(HTS221_CTRL_REG2, 1, 0x1, 1);
+    if (obj->IO.write_reg_IT_driven) {
+        HTS221_Write_Reg_Cplt_Callback();
+    }
+}
+
+void HTS221_Read(HTS221_Obj *obj) {
+    obj->state = HTS221_READING;
+    obj->IO->read_reg(HTS221_HUMIDITY_OUT_L, 1, obj->registers.HUMIDITY_OUT_L, 4);
+    if (obj->IO.read_reg_IT_driven) {
+        HTS221_Read_Reg_Cplt_Callback();
+    }
 }
--- HTS221/hts221.h
+++ HTS221/hts221.h
@@ -8,6 +8,10 @@
 #include <stdint.h>
 #include "commons.h"
 
+// IMPORTANT
+// Call the exported reg_write_cplt and reg_read_cplt callbacks from their respective interrupt handlers if using IT driven IO
+
+#define HTS221_SAD             0x5F
 #define HTS221_WHO_AM_I        0x0F
 #define HTS221_AV_CONFR        0x10
 #define HTS221_CTRL_REG1       0x20
@@ -20,33 +24,75 @@
 #define HTS221_TEMP_OUT_H      0x2B
 #define HTS221_CALIB_0TOF      0x30
 
+typedef enum {
+    HTS221_POWEROFF = 0x0,
+    HTS221_POWERON  = 0x1
+} HTS221_PoweredTypeDef;
+
+typedef enum {
+    HTS221_AVGT2   = 0x0,
+    HTS221_AVGT4   = 0x1,
+    HTS221_AVGT8   = 0x2,
+    HTS221_AVGT16  = 0x3,
+    HTS221_AVGT32  = 0x4,
+    HTS221_AVGT64  = 0x5,
+    HTS221_AVGT128 = 0x6,
+    HTS221_AVGT256 = 0x7
+} HTS221_AVGTTypeDef;
+
+typedef enum {
+    HTS221_AVGH4   = 0x0,
+    HTS221_AVGH8   = 0x1,
+    HTS221_AVGH16  = 0x2,
+    HTS221_AVGH32  = 0x3,
+    HTS221_AVGH64  = 0x4,
+    HTS221_AVGH128 = 0x5,
+    HTS221_AVGH256 = 0x6,
+    HTS221_AVGH512 = 0x7
+} HTS221_AVGTTypeDef;
+
+typedef enum {
+    BDU_continuous = 0x0,
+    BDU_synced     = 0x1
+} HTS221_BDUTypeDef;
+
+typedef enum {
+    HTS221_ODR_OS = 0x0,
+    HTS221_1HZ    = 0x1,
+    HTS221_7HZ    = 0x2,
+    HTS221_12HZ   = 0x3
+} HTS221_ODRTypeDef;
+
 typedef struct {
-    uint8_t RESERVED00[15];
-    uint8_t WHO_AM_I;
-    uint8_t AV_CONF;
-    uint8_t RESERVED11[12];
-    uint8_t CTRL_REG1;
-    uint8_t CTRL_REG2;
-    uint8_t CTRL_REG3;
-    uint8_t RESERVED23[4];
-    uint8_t STATUS_REG;
-    uint8_t HUMIDITY_OUT_L;
-    uint8_t HUMIDITY_OUT_H;
-    uint8_t TEMP_OUT_L;
-    uint8_t TEMP_OUT_H;
-    uint8_t RESERVED2C[4];
-    uint8_t CALIB_0TOF[16];
-} HTS221_Reg;
+    HTS221_DRDY_DISABLED = 0x0,
+    HTS221_DRDY_ENABLED = 0x1,
+} HTS221_DRDYTypeDef;
+
+typedef struct {
+    HTS221_PoweredTypeDef powered;
+    HTS221_AVGTTypeDef tempRes;
+    HTS221_AVGHTypeDef humRes;
+    HTS221_BDUTypeDef bdu;
+    HTS221_ODRTypeDef odr;
+    HTS221_DRDYTypeDef drdy;
+} HTS221_SettingsTypeDef;
 
 typedef struct {
     StatusTypeDef (*read_reg)(uint16_t memAddress, uint8_t memSize, uint8_t *data, uint16_t dataSize);
     StatusTypeDef (*write_reg)(uint16_t memAddress, uint8_t memSize, uint8_t *data, uint16_t dataSize);
+    uint8_t read_reg_IT_driven;
+    uint8_t write_reg_IT_driven;
 } HTS221_IO_Object;
 
+typedef enum {
+    HTS221_INITIALIZING = 0x0,
+    HTS221_CONFIGURING  = 0x1,
+    HTS221_REQUESTING   = 0x2,
+    HTS221_READING      = 0x3,
+    HTS221_READY        = 0x4
+} HTS221_StateTypeDef;
+
 typedef struct {
-    uint8_t SAD;
-    HTS221_Reg *registers;
-    Peripheral_IO_Object *IO;
     int16_t T0_degC;
     int16_t T1_degC;
     int16_t T0_out;
@@ -55,11 +101,37 @@ typedef struct {
     uint16_t H1_rH;
     int16_T H0_out;
     int16_t H1_out;
+} HTS221_CalibrationValuesTypeDef;
+
+typedef struct {
+    uint8_t HUMIDITY_OUT_L;
+    uint8_t HUMIDITY_OUT_H;
+    uint8_t TEMP_OUT_L;
+    uint8_t TEMP_OUT_H;
+    uint8_t CALIB_0TOF[16];
+} HTS221_RegisterTypeDef;
+
+typedef struct {
+    uint8_t SAD;
+    HTS221_StateTypeDef state;
+    HTS221_SettingsTypeDef settings;
+    HTS221_RegisterTypeDef registers;
+    Peripheral_IO_Object *IO;
+    HTS221_CalibrationValuesTypeDef calibrations;
     int16_t temperature;
     uint16_t humidity;
 } HTS221_Obj;
 
-HTS221_Init(HTS221_Obj *obj, Peripheral_IO_Object *io);
-HTS221_Read(HTS221_Obj *obj);
+void HTS221_Init(HTS221_Obj *obj, Peripheral_IO_Object *io);
+void HTS221_SetPowered(HTS221_Obj *obj, HTS221_PoweredTypeDef powered);
+void HTS221_SetResolution(HTS221_Obj *obj, HTS221_AVGTTypeDef tempRes, HTS221_AVGHTypeDef humRes);
+void HTS221_SetBDU(HTS221_Obj *obj, HTS221_BDUTypeDef bdu);
+void HTS221_SetODR(HTS221_Obj *obj, HTS221_ODRTypeDef odr);
+void HTS221_SetDRDY(HTS221_Obj *obj, HTS221_DRDYTypeDef drdy);
+void HTS221_RequestReading(HTS221_Obj *obj);
+void HTS221_Read(HTS221_Obj *obj);
+
+void HTS221_Read_Reg_Cplt_Callback(HTS221_Obj *obj);
+void HTS221_Write_Reg_Cplt_Callback(HTS221_Obj *obj);
 
 #endif //HOMEMONITOR_HTS221_H
