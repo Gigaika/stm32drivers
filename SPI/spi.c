95f1a5f1a2df6cfe584360145157b08758b0f987
trouverun <32040883+trouverun@users.noreply.github.com>
2021-02-02 19:13:42 +0200

    Delete SPI directory

--- SPI/spi.c
+++
@@ -1,5 +0,0 @@
-//
-// Created by aleksi on 19.8.2020.
-//
-
-#include "spi.h"
--- SPI/spi.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by aleksi on 19.8.2020.
-//
-
-#ifndef STM32DRIVERS_SPI_H
-#define STM32DRIVERS_SPI_H
-
-#endif //STM32DRIVERS_SPI_H
